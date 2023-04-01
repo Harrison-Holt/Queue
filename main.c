c365c540d36d9395c98cdcd33f3c60c8e227f6a7
Harrison Holt <116909429+Harrison-Holt@users.noreply.github.com>
2023-04-01 11:16:26 -0400

    Create main.c

---
+++ main.c
@@ -0,0 +1,23 @@
+/** 
+ * queue in C
+ * March 2, 2023
+**/ 
+
+#include <stdio.h>
+#include "queue.h"
+int main()
+{
+    push(); 
+    push();
+    push(); 
+    push(); 
+    push(); 
+    
+    show(); 
+    
+    pop(); 
+    
+    show(); 
+    
+    return 0;
+}
